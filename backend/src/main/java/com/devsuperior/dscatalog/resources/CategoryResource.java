5ecffb6bd3a06a6a00829c2de58a53095ffa3d5c
Henrique Sato <henriquesato2003@gmail.com>
2022-10-24 12:44:18 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.resources;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public List<Category> findAll() {
-        return service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
     }
 }
